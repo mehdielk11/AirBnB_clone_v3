04423791e0e8077b65c531425327ddada88f23a8
mehdielk11 <elkhemlichi.mehdi@gmail.com>
2023-10-29 07:28:42 -0700

    fix

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,7 +2,7 @@
 """Initialize files"""
 from flask import Blueprint
 
-app_views = Blueprint("app_views", __name__,  url_prefix="/api/v1")
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
 from api.v1.views.users import *
 from api.v1.views.amenities import *
@@ -11,3 +11,4 @@ from api.v1.views.states import *
 from api.v1.views.index import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
+
--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,86 +1,59 @@
 #!/usr/bin/python3
-"""States views"""
-from flask import jsonify, make_response, abort, request
+"""User views"""
+from flask import jsonify, abort, request
 from api.v1.views import app_views
 from models import storage
 from models.user import User
 
-
-@app_views.route('/users',
-                 strict_slashes=False,
-                 methods=['GET', 'POST'])
+@app_views.route('/users', strict_slashes=False, methods=['GET', 'POST'])
 def view_users():
-    """Returns the list of all User objects"""
     if request.method == 'POST':
-        # Get the attributes from the request
         data = request.get_json()
 
-        if isinstance(data, dict):
-            pass
-        else:
+        if data is None:
             return jsonify({"error": "Not a JSON"}), 400
 
-        if 'id' in data.keys():
-            data.pop("id")
-        if 'created_at' in data.keys():
-            data.pop("created_at")
-        if 'updated_at' in data.keys():
-            data.pop("updated_at")
-
-        if 'email' not in data.keys():
+        if 'email' not in data:
             return jsonify({'error': 'Missing email'}), 400
-        if 'password' not in data.keys():
+        if 'password' not in data:
             return jsonify({'error': 'Missing password'}), 400
 
-        # Create the object
-        obj = User(**data)
-
-        # Save the object in storage
-        storage.new(obj)
+        new_user = User(**data)
+        storage.new(new_user)
         storage.save()
-        return jsonify(obj.to_dict()), 201
+        return jsonify(new_user.to_dict()), 201
 
     if request.method == 'GET':
-        users = storage.all("User")
-        list = []
-        for name, user in users.items():
-            list.append(user.to_dict())
-        return jsonify(list)
+        users = storage.all(User)
+        user_list = [user.to_dict() for user in users.values()]
+        return jsonify(user_list)
 
-
-@app_views.route('/users/<id>',
-                 strict_slashes=False,
-                 methods=['GET', 'DELETE', 'PUT'])
-def view_user_id(id):
-    """Returns a list of all User objects, or delete an
-    object if a given id
-    """
-    user = storage.get(User, id)
+@app_views.route('/users/<user_id>', strict_slashes=False, methods=['GET', 'DELETE', 'PUT'])
+def view_user(user_id):
+    user = storage.get(User, user_id)
 
     if user is None:
-        return abort(404)
+        abort(404)
 
     if request.method == 'GET':
-
         return jsonify(user.to_dict())
 
     if request.method == 'DELETE':
-
         storage.delete(user)
         storage.save()
         return jsonify({}), 200
 
     if request.method == 'PUT':
         data = request.get_json()
-        if isinstance(data, dict):
-            pass
-        else:
+
+        if data is None:
             return jsonify({"error": "Not a JSON"}), 400
 
+        # Update user object
         for key, value in data.items():
             if key not in ["id", "email", "created_at", "updated_at"]:
                 setattr(user, key, value)
-        # This save is possibly not working to obtain
-        # permanent changes in the data base
+
         storage.save()
         return jsonify(user.to_dict())
+
